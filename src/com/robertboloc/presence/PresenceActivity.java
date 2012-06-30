51a9d4245e90aff5c1dd366864fa92145d90c81e
Robert Boloc <robertboloc@gmail.com>
2012-06-30 14:44:09 +0200

    added settings and menu

--- src/com/robertboloc/presence/PresenceActivity.java
+++ src/com/robertboloc/presence/PresenceActivity.java
@@ -1,7 +1,11 @@
 package com.robertboloc.presence;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,5 +28,27 @@ public class PresenceActivity extends Activity {
         ListView mainMenu = (ListView)findViewById(R.id.mainMenu);
         	 
         mainMenu.setAdapter(menuAdapter);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.incidence:
+                //TODO: show incindence screen
+                return true;
+            case R.id.settings:
+            	startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
